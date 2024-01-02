373c653b8d9ebd426daae1ed6530b2d7431fda00
Allyson Athyrson <allysonathyrson.work@gmail.com>
2024-01-02 18:39:10 -0300

    ingredients (feat): config to create

--- src/ingredients/ingredients.module.ts
+++ src/ingredients/ingredients.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { IngredientsService } from './ingredients.service';
 import { IngredientsController } from './ingredients.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Ingredient } from './entities/ingredient.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Ingredient]), IngredientsModule],
   controllers: [IngredientsController],
   providers: [IngredientsService],
 })
--- src/ingredients/ingredients.service.ts
+++ src/ingredients/ingredients.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { CreateIngredientDto } from './dto/create-ingredient.dto';
 import { UpdateIngredientDto } from './dto/update-ingredient.dto';
+import { Ingredient } from './entities/ingredient.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class IngredientsService {
-  create(createIngredientDto: CreateIngredientDto) {
-    return 'This action adds a new ingredient';
+  constructor(
+    @InjectRepository(Ingredient)
+    private readonly ingredientRepository: Repository<Ingredient>,
+  ) {}
+
+  async create(createIngredientDto: CreateIngredientDto) {
+    const ingredient: Ingredient = new Ingredient();
+
+    ingredient.name = createIngredientDto.name;
+    ingredient.quantity = createIngredientDto.quantity;
+
+    return await this.ingredientRepository.save(ingredient);
   }
 
   findAll() {
