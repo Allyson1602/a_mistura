4c9b7d14561d636a19ebe014bb70fee8358e2ff3
Allyson Athyrson <allysonathyrson.work@gmail.com>
2024-01-09 17:52:25 -0300

    update

--- dist/main.js
+++ dist/main.js
@@ -1,18 +1,19 @@
-Object.defineProperty(exports, '__esModule', { value: true });
-const core_1 = require('@nestjs/core');
-const app_module_1 = require('./app.module');
-const helmet_1 = require('helmet');
-const common_1 = require('@nestjs/common');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+const helmet_1 = require("helmet");
+const common_1 = require("@nestjs/common");
 async function bootstrap() {
-  const app = await core_1.NestFactory.create(app_module_1.AppModule);
-  app.enableCors({ origin: ['http://localhost:5173'] });
-  app.use((0, helmet_1.default)());
-  app.useGlobalPipes(new common_1.ValidationPipe());
-  await app.listen(3000);
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors({ origin: ['http://localhost:5173'] });
+    app.use((0, helmet_1.default)());
+    app.useGlobalPipes(new common_1.ValidationPipe());
+    await app.listen(3000);
+    if (module.hot) {
+        module.hot.accept();
+        module.hot.dispose(() => app.close());
+    }
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
