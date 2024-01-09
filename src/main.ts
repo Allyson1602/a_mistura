da63873195482307f7913d65ff633c4270d8c473
Allyson Athyrson <allysonathyrson.work@gmail.com>
2024-01-09 17:45:18 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,18 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { ValidationPipe } from '@nestjs/common';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: ['http://localhost:5173'] });
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
