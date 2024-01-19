9818f6b9e5767eb18636b0a418637b208bec32f2
Allyson Athyrson <allysonathyrson.work@gmail.com>
2024-01-19 15:21:56 -0300

    (feat): config vercel

--- src/data-source.ts
+++ src/data-source.ts
@@ -15,6 +15,7 @@ export const DataSource: DataSourceOptions = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: true,
   entities: [
     Ingredient,
     Plate,
@@ -24,7 +25,4 @@ export const DataSource: DataSourceOptions = {
     IngredientPlate,
   ],
   synchronize: true,
-  ssl: {
-    ca: readFileSync('us-east-1-bundle.pem').toString(),
-  },
 };
