6c6e8e15c5d9b02a181123fd079a95062962f724
Allyson Athyrson <allysonathyrson.work@gmail.com>
2024-02-01 10:18:55 -0300

    Revert "(refactor): fix db connection with aws rds"
    This reverts commit 4db5bfc81c4d6fc867677b325f79ae08594d8c90.

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,7 +6,6 @@ import { Plate } from './plates/entities/plate.entity';
 import { Instruction } from './instructions/entities/instruction.entity';
 import { ImagePlate } from './images-plates/entities/image-plate.entity';
 import { IngredientPlate } from './ingredient-plates/entities/ingredient-plate.entity';
-import { readFileSync } from 'fs';
 
 export const DataSource: DataSourceOptions = {
   type: 'postgres',
@@ -15,6 +14,7 @@ export const DataSource: DataSourceOptions = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: true,
   entities: [
     Ingredient,
     Plate,
@@ -24,7 +24,4 @@ export const DataSource: DataSourceOptions = {
     IngredientPlate,
   ],
   synchronize: true,
-  ssl: {
-    ca: readFileSync('us-east-1-bundle.pem').toString(),
-  },
 };
