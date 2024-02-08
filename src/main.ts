c9f377e628520f4000ddf7fae52b15357863c561
Allyson Athyrson <allysonathyrson.work@gmail.com>
2024-02-08 16:53:30 -0300

    (refactor): fix run handler

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,29 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { ValidationPipe } from '@nestjs/common';
+import serverlessExpress from '@codegenie/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
 
-async function bootstrap() {
+let server: Handler;
+
+async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['https://a-mistura-ca8y8djn6-allyson1602.vercel.app'],
-  });
+  app.enableCors({ origin: [] });
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
 }
-bootstrap();
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
+  const response = await server(event, context, callback);
+
+  return response;
+};
