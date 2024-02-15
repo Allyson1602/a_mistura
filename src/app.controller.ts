335547187a4d83b2464567c1e777242070ecd5ab
Allyson Athyrson <allysonathyrson.work@gmail.com>
2024-02-15 17:06:38 -0300

    (refactor): clean app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,4 @@
 import { Controller } from '@nestjs/common';
-import { AppService } from './app.service';
 
-@Controller('hello')
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-}
+@Controller()
+export class AppController {}
